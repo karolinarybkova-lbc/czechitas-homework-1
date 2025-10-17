a912a2d54cf6e3025dfa7535924bc9e4721ea50d
Karolina <karolina.minarikova@heureka.cz>
2025-10-17 22:37:02 +0200

    Added dot at the end of the sentence

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ const fahrenheit = Number(prompt("Zadejte teplotu ve Fahrenheitech:"));
 const celsius = (fahrenheit - 32) * 5 / 9;
 const celsiusRounded = Math.round(celsius / 5) * 5;
 
-document.body.innerHTML += "<p>Teplota v Celsiích je " + celsiusRounded + " °C</p>";
+document.body.innerHTML += "<p>Teplota v Celsiích je " + celsiusRounded + " °C.</p>";
