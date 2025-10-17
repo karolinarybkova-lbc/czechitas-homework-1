e82a893ab25c5f81a71cdb6acf79bff439b2d622
Karolina <karolina.minarikova@heureka.cz>
2025-10-17 22:30:10 +0200

    Homework1

---
+++ script.js
@@ -0,0 +1,5 @@
+const fahrenheit = Number(prompt("Zadejte teplotu ve Fahrenheitech:"));
+const celsius = (fahrenheit - 32) * 5 / 9;
+const celsiusRounded = Math.round(celsius / 5) * 5;
+
+document.body.innerHTML += "<p>Teplota v Celsiích je " + celsiusRounded + " °C</p>";
